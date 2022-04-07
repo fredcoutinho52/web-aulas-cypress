e505cb9a0ba1391783d893ee277cc00180fc7012
Fred Coutinho <fredcoutinho52@gmail.com>
2022-04-07 11:46:16 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import api from "./api";
+import "./global.css";
+import "./styles.css";
 
 function App() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [dataNascimento, setDataNascimento] = useState("");
+  const [genero, setGenero] = useState("");
+
+  async function enviar(e) {
+    e.preventDefault();
+    const data = { nome, sobrenome, email, telefone, dataNascimento, genero };
+    const response = await api.post("/formulario", data);
+    alert(response.data.message);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form>
+        <div className="input-group">
+          <label htmlFor="nome">Nome</label>
+          <input type="text" name="nome" onChange={e => setNome(e.target.value)} />
+        </div>
+        <div className="input-group">
+          <label htmlFor="sobrenome">Sobrenome</label>
+          <input type="text" name="sobrenome" onChange={e => setSobrenome(e.target.value)} />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" onChange={e => setEmail(e.target.value)} />
+        </div>
+        <div className="input-group">
+          <label htmlFor="telefone">Telefone</label>
+          <input type="text" name="telefone" onChange={e => setTelefone(e.target.value)} />
+        </div>
+        <div className="input-group">
+          <label htmlFor="data-nascimento">Data de Nascimento</label>
+          <input type="text" name="data-nascimento" onChange={e => setDataNascimento(e.target.value)} />
+        </div>
+        <div className="input-group">
+          <label htmlFor="genero">Gênero</label>
+          <input type="text" name="genero" onChange={e => setGenero(e.target.value)} />
+        </div>
+
+        <div className="button-container">
+          <button onClick={enviar}>Enviar</button>
+        </div>
+      </form>
     </div>
   );
 }
